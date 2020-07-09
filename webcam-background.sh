5e0bb1ba523d02c8340d9cc2d32db3957056c343
Manuel Simon <manuel.simon@tum.de>
2020-07-10 00:40:23 +0200

    set display variable accordingly

--- webcam-background.sh
+++ webcam-background.sh
@@ -5,6 +5,7 @@ if [[ "$ssid" == "Galaxy S108610" ]]; then
 	exit 1
 fi
 
+export DISPLAY=:0
 wget -O ~/.background "https://www.foto-webcam.eu//webcam/freimann/current/1920.jpg"  > /dev/null
 feh --bg-scale ~/.background
 
