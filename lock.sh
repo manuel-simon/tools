5615f85b3c938ab518170a7fdb5f38a6f8800cd8
Manuel Simon <manuel.simon@tum.de>
2025-11-21 11:58:16 +0100

    update

--- lock.sh
+++ lock.sh
@@ -20,12 +20,14 @@ SCREENSHOT="scrot $IMAGE" # 0.46s
 # Uncomment one to use, if you have multiple, the last one will be used
 
 # All options are here: http://www.imagemagick.org/Usage/blur/#blur_args
-BLURTYPE="0x5" # 7.52s
+#BLURTYPE="0x5" # 7.52s
 #BLURTYPE="0x2" # 4.39s
 #BLURTYPE="5x2" # 3.80s
 #BLURTYPE="2x8" # 2.90s
 #BLURTYPE="2x3" # 2.92s
 #BLURTYPE="3x10"
+BLURTYPE="14x14"
+
 
 # Get the screenshot, add the blur and lock the screen with it
 $SCREENSHOT
--- webcam-background.sh
+++ webcam-background.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-
+set -x
 PARAM=$1
 
 ssid=$(iwgetid -r)
 if [[ "$ssid" == "AndroidAP_4347" && "$PARAM" != "force" ]]; then
 	exit 1
 fi
+DISPLAY=:0.0
+#export DISPLAY=:0.0
+echo $DISPLAY
 
-export DISPLAY=:0
-wget -O ~/.background "https://www.foto-webcam.eu/webcam/tum-olympiapark/current/1920.jpg"  > /dev/null
-feh --bg-scale ~/.background
+#wget -O ~/.background "https://www.foto-webcam.eu/webcam/tum-olympiapark/current/1920.jpg"  > /dev/null
+wget -O ~/.background "https://www.terra-hd.de/deutschesmuseum/img/current/3840.jpg"  > /dev/null
+DISPLAY=:0.0 feh --bg-scale ~/.background
 
